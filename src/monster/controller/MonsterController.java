96690c345716316bbf42d71a808df58e1400bda3
Wailex <mwag0654@740s-pl-103.local>
2016-09-14 08:59:47 -0600

    Made the monster class constructor

---
+++ src/monster/controller/MonsterController.java
@@ -0,0 +1,13 @@
+package monster.controller;
+
+public class MonsterController 
+{
+	
+	public static void main(String[] args)
+	{
+		MonsterController monsterAppController = new MonsterController();
+		monsterAppControler.start();
+				
+	}
+
+}
