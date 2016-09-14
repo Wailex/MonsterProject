0332856078df66882e759131999756ded0b12d1c
Wailex <mwag0654@740s-pl-103.local>
2016-09-14 09:49:37 -0600

    Added print statements about monster

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,13 +1,26 @@
 package monster.controller;
-
+import monster.model.MarshmallowMonster;
 public class MonsterController 
 {
-	
-	public static void main(String[] args)
+	private MarshmallowMonster firstMonster;
+	public MonsterController()
 	{
-		MonsterController monsterAppController = new MonsterController();
-		monsterAppControler.start();
-				
+		firstMonster = new MarshmallowMonster("BB Steve",2,2,false,2,2);
 	}
-
+	/*name bb steve, 
+	  * 2 anntenas 
+	  * 2 eyes 
+	  * no belly button 
+	  * 2 arms 
+	  * 2 legs  */
+	public void start()
+	{
+		System.out.println("Here is my monster:"+firstMonster);
+		System.out.println("My monster has this many eyes:"+firstMonster.getEyeCount());
+		if(firstMonster.getEyeCount()>3)
+		{
+			System.out.println("Thats a lot of eyes!!!!-Sacry");
+		}
+	}
+	
 }
